271810843fc5dda8bd1c16ac9086ee3168237a15
lanhlanh <a1k9lanh@gmail.com>
2019-06-03 08:46:16 +0700

    change db config

--- config/puma.rb
+++ config/puma.rb
@@ -78,8 +78,9 @@ activate_control_app
 
 on_worker_boot do
   require "active_record"
-  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+  # ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  # ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 end
 
 # Allow puma to be restarted by `rails restart` command.
