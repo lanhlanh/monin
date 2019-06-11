cd78527ae9bce75ddb85271b7f52b70cf16f3411
lanhlanh <a1k9lanh@gmail.com>
2019-06-11 16:13:02 +0700

    add secret_key_base in production enviroment config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.secret_key_base = Figaro.env.secret_key_base
 end
